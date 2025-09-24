02c4f5b81e7b0b0feda56774f4858c1e1763e276
eunicean <eunice.mata2992@gmail.com>
2025-09-24 15:14:55 -0600

    parte 2

---
+++ Parte2.c
@@ -0,0 +1,19 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Proceso %d en la primera etapa\n", rank);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printf("Proceso %d en la segunda etapa\n", rank);
+
+    MPI_Finalize();
+    return 0;
+}
