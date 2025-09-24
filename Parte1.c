dea16060bb1423d09c31ba4500085aa5545fc8bc
eunicean <eunice.mata2992@gmail.com>
2025-09-24 15:14:19 -0600

    parte 1

---
+++ Parte1.c
@@ -0,0 +1,19 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        printf("Rank %d comunicador\n", rank);
+    } else {
+        printf("Rank %d con size %d\n", rank, size);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
